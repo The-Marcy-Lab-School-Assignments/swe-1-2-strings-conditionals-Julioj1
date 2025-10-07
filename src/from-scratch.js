c556d62613af843b5790b124d6ccff19839cd30a
Julioj1 <dbjulioj@gmail.com>
2025-10-07 14:39:51 -0400

    Completed scratch.js

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -13,24 +13,79 @@ const measureRain = () => {
 };
 
 const happyBirthdayPet = () => {
+  if (breed === 'snake') {
+    return "Hiss hiss!";
+  } else if (breed === "cat" && age < 5) {
+    return 'Mew mew!';
+  } else if (breed === "cat" && age >= 5) {
+    return "Meow meow!";
+  } else if (breed === "dog" && age < 5) {
+    return "Arf arf!";
+  } else if (breed === "dog" && age >= 5 && age < 10) {
+    return "Woof woof!";
+  } else if (breed === "dog" && age >= 10) {
+    return 'Boof!';
+  } else {
+    return 'Happy birthday!';
+  }
 };
 
 const funTypes = () => {
+  if (typeof jsType === "string") {
+    return "That's just some text.";
+  } else if (Number.isNaN(jsType)) {
+    return "Well, now you're just showing off.";
+  } else if (typeof jsType === "number") {
+    return "That's a good number.";
+  } else if (typeof jsType === "boolean") {
+    return "To bool, or not to bool?";
+  } else if (typeof jsType === "undefined") {
+    return "Nothing, but I didn't set that.";
+  } else if (jsType === null) {
+    return "Nothing, and I did set that.";
+  } else if (Array.isArray(jsType)) {
+    return "I order you to be indexed.";
+  } else if (typeof jsType === "object") {
+    return "Anybody got the key?";
+  } else {
+    return "Not a valid data type";
+  }
 };
 
 const rounder = () => {
+  if (roundingSetting === "up") {
+    return Math.ceil(float);
+  } else if (roundingSetting === "down") {
+    return Math.floor(float);
+  } else if (roundingSetting === "honest") {
+    return Math.round(float);
+  };
 };
 
 const formatName = () => {
+  let formattedFirst = first.charAt(0).toUpperCase() + first.slice(1).toLowerCase();
+  let formattedLast = last.charAt(0).toUpperCase() + last.slice(1).toLowerCase();
+  return `${formattedFirst} ${formattedLast}`;
 };
 
 const extractDomain = () => {
+  return email.split("@").pop();
 };
 
 const startsWithVowel = () => {
+  let vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
+  if (vowels.includes(str[0])) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const rotate = () => {
+  for (let i = 0; i < num; i++) {
+    str = str[str.length - 1] + str.slice(0, str.length - 1)
+  }
+  return str;
 };
 
 module.exports = {
