6e007ee79f28d593b6e760866c30644a4814a286
Julioj1 <dbjulioj@gmail.com>
2025-10-07 14:35:28 -0400

    Completed measureRain

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,4 +1,15 @@
 const measureRain = () => {
+  if (inches <= 0) {
+    return "drought";
+  } else if (inches < 2) {
+    return "dry";
+  } else if (inches < 4) {
+    return "average";
+  } else if (inches < 6) {
+    return "rainy";
+  } else {
+    return "flood";
+  }
 };
 
 const happyBirthdayPet = () => {
